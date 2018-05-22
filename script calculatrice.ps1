2f701893f721bb28eb12ff8cb244d9ad292ff25c
kebabfrite <cryzer16@msn.com>
2018-05-22 14:42:24 +0200

    trop baleze

---
+++ script calculatrice.ps1
@@ -0,0 +1,12 @@
+﻿[int]$valeur1 = read-host "entrer un nombre"
+$operateur = read-host "choisissez un operateur"
+[int]$valeur2 = read-host "entrer un nombre"
+
+switch ($operateur) {
+"+" {$result = $valeur1 + $valeur2}
+"-" {$result = $valeur1 - $valeur2}
+"/" {$result = $valeur1 / $valeur2}
+"*" {$result = $valeur1 * $valeur2}
+}
+
+Write-Output $result
